cc98f7b6dc2b4cca740376cc47da3e3ddd1e3f3f
MaTTalv001 <pomeranian1981@gmail.com>
2024-05-02 14:13:52 +0900

    URL修正

--- frontend/src/config/settings.js
+++ frontend/src/config/settings.js
@@ -1,3 +1,7 @@
 export const API_URL =
   process.env.REACT_APP_API_URL ||
   "https://mao-the-3rd-day-760b5f4d11f2.herokuapp.com";
+
+export const FRONT_URL =
+  process.env.REACT_APP_BASE_URL ||
+  "https://mao-the-3rd-day-front-e8eb3156d3f2.herokuapp.com";
