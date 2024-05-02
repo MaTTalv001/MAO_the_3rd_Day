0b1b50287e5bd94d7cc68788086088f7f2577e55
MaTTalv001 <pomeranian1981@gmail.com>
2024-05-02 12:49:47 +0900

    ユーザー詳細ページ

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,11 +12,9 @@ export const HomePage = () => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get("token");
-
     if (token) {
       setToken(token);
       localStorage.setItem("auth", token);
-      /* navigate(RoutePath.Home.path);*/
       console.log(currentUser);
     }
   }, [setToken, navigate]);
@@ -25,32 +23,74 @@ export const HomePage = () => {
     e.preventDefault();
     const form = document.createElement("form");
     form.method = "GET";
-    form.action = `${API_URL}/auth/google_oauth2`; // Google認証のエンドポイントURL
+    form.action = `${API_URL}/auth/google_oauth2`;
     document.body.appendChild(form);
     form.submit();
   };
 
   return (
-    <article className="pt-12 pb-12 max-w-[65%] m-auto">
-      <div className="bg-white rounded p-[50px]">
-        <div className="p-15">
-          <section className="text-center">
-            <div className="block mb-15">
-              <h2 className="text-4xl font-semibold">Google認証</h2>
-            </div>
-            <div className="text-center">
+    <div className="min-h-screen bg-base-200 flex flex-col items-center justify-center">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="flex justify-center">
+            <img
+              src="imgs/title/title.png"
+              alt="App Title"
+              className="max-w-full h-auto"
+            />
+          </div>
+          <div className="flex flex-col justify-center">
+            <h2 className="text-4xl font-bold mb-4">3日目に魔王がいる</h2>
+            <p className="text-xl mb-8">MAO the 3rd Day</p>
+            <div className="space-y-4">
               <button
-                className="bg-gray-800 text-white py-2.5 px-5 rounded my-4"
+                className="btn btn-accent gap-2 w-full"
                 onClick={handleGoogleAuth}
               >
-                <div className="flex items-center justify-center">
-                  <span className="text-2xl">Googleでログインする</span>
-                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 48 48"
+                >
+                  <path
+                    fill="#FFC107"
+                    d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                  ></path>
+                  <path
+                    fill="#FF3D00"
+                    d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                  ></path>
+                  <path
+                    fill="#4CAF50"
+                    d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                  ></path>
+                  <path
+                    fill="#1976D2"
+                    d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                  ></path>
+                </svg>
+                Googleログイン
+              </button>
+              <button className="btn btn-accent gap-2 w-full">
+                <img
+                  src="imgs/title/guest_login.png"
+                  alt="Guest Login"
+                  className="w-6 h-6"
+                />
+                ゲストログイン
               </button>
             </div>
-          </section>
+          </div>
         </div>
       </div>
-    </article>
+      <div className="container mx-auto px-4 mt-8">
+        <div className="bg-base-100 p-4 rounded-lg">
+          <p className="text-center text-base-content">
+            インストラクション（仮置き）
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
--- frontend/src/pages/UsersShow.jsx
+++ frontend/src/pages/UsersShow.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAuth } from "../providers/auth";
 import { API_URL } from "../config/settings";
+import { Link } from "react-router-dom";
 
 export const UsersShow = () => {
   const { currentUser } = useAuth();
@@ -20,18 +21,67 @@ export const UsersShow = () => {
   }
 
   return (
-    <>
-      <h1>ユーザー詳細</h1>
-      <div>
-        {user.avatars.map((avatar) => (
-          <img src={avatar.avatar_url} alt="User Avatar" key={avatar.id} />
-        ))}
-        <img src={user.latest_avatar_url} alt="User Avatar" />
-        <h2>冒険者名：{user.nickname}</h2>
-        <h2>レベル：{user.latest_status.level}</h2>
-        <h2>HP：{user.latest_status.hp}</h2>
-        <h2>3日達成数：{user.achievement}</h2>
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-4xl font-bold">{user.nickname}の詳細ページ</h1>
+        <Link to={`/users`} className="btn btn-ghost btn-sm">
+          一覧にもどる
+        </Link>
       </div>
-    </>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {/* プロフィール */}
+        <div className="bg-base-200 p-4 rounded-lg">
+          <h2 className="text-2xl font-bold mb-2">{user.nickname}</h2>
+          <img
+            src={`${process.env.REACT_APP_BASE_URL}/${user.latest_avatar_url}`}
+            alt="User Avatar"
+            className="w-full h-auto mb-4 rounded-lg"
+          />
+        </div>
+
+        {/* ステータス */}
+        {user.latest_status && (
+          <div className="bg-base-200 p-4 rounded-lg">
+            <h2 className="text-xl font-bold mb-2">ステータス</h2>
+            <p>Level: {user.latest_status.level}</p>
+            <p>HP: {user.latest_status.hp}</p>
+            <p>体力: {user.latest_status.strength}</p>
+            <p>知力: {user.latest_status.intelligence}</p>
+            <p>精神: {user.latest_status.wisdom}</p>
+            <p>素速さ: {user.latest_status.dexterity}</p>
+            <p>カリスマ: {user.latest_status.charisma}</p>
+            <p className="mt-4">3日達成数: {user.achievement ?? 0}</p>
+            <div className="mt-4 bg-base-300 p-2 rounded-lg">
+              <h3 className="text-lg font-bold mb-2">プロフィール</h3>
+              <p>{user.profile ?? "未設定"}</p>
+            </div>
+          </div>
+        )}
+
+        {/* 歴代アバター */}
+        <div className="bg-base-200 p-4 rounded-lg">
+          <h2 className="text-xl font-bold mb-2">歴代アバター</h2>
+          <div className="grid grid-cols-3 gap-2">
+            {user.avatars.map((avatar) => (
+              <img
+                key={avatar.id}
+                src={`${process.env.REACT_APP_BASE_URL}/${avatar.avatar_url}`}
+                alt="User Avatar"
+                className="w-full h-auto rounded"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* 活動一覧 */}
+      <div className="bg-base-200 mt-4 p-4 rounded-lg">
+        <h2 className="text-xl font-bold mb-2">活動一覧</h2>
+        {/* 仮のボックスを表示 */}
+        <div className="bg-base-300 h-40 rounded-lg flex items-center justify-center">
+          <p className="text-lg">活動一覧（仮）</p>
+        </div>
+      </div>
+    </div>
   );
 };
