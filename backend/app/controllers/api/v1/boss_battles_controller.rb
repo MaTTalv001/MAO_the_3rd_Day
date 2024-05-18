a5ecbd44b62de7390a7ba2b2629a3ef95b65dc85
MaTTalv001 <pomeranian1981@gmail.com>
2024-05-18 10:15:45 +0900

    魔王戦のロジック完成

--- backend/app/controllers/api/v1/boss_battles_controller.rb
+++ backend/app/controllers/api/v1/boss_battles_controller.rb
@@ -1,22 +1,24 @@
-class BossBattlesController < ApplicationController
-  def create
-    @boss = Boss.find(params[:boss_id])
-    damage = params[:damage].to_i
-    result = params[:result] # 勝ち負けの結果を受け取る
+module Api
+  module V1
+    class BossBattlesController < ApplicationController
+      def create
+        @boss = Boss.find(params[:boss_id])
+        damage = params[:damage].to_i
+        result = params[:result] # 勝ち負けの結果を受け取る
 
-    # 累積ダメージを計算
-    total_damage = @boss.total_damage_dealt_by(current_user) + damage
-    remaining_hp = @boss.hp - total_damage
+      # 累積ダメージを計算
+        total_damage = @boss.total_damage_dealt_by(current_user) + damage
+        remaining_hp = @boss.hp - total_damage
 
-    # ボスバトルログの作成
-    BossBattleLog.create!(
-      user_id: current_user.id,
-      boss_id: @boss.id,
-      damage_dealt: damage,
-      result: result
-    )
+        # ボスバトルログの作成
+        BossBattleLog.create!(
+        user_id: current_user.id,
+        boss_id: @boss.id,
+        damage_dealt: damage,
+        result: result
+       )
 
 
-    render json: { remaining_hp: remaining_hp }
+      render json: { remaining_hp: remaining_hp }
+    end
   end
-end
