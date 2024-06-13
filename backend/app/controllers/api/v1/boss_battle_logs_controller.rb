5c7f6b61d1f6b1049f6b11f5b4430dd3e461fd7c
MaTTalv001 <pomeranian1981@gmail.com>
2024-06-13 23:09:26 +0900

    ボス累積ダメージの修正

--- backend/app/controllers/api/v1/boss_battle_logs_controller.rb
+++ backend/app/controllers/api/v1/boss_battle_logs_controller.rb
@@ -11,33 +11,41 @@ class BossBattleLogsController < ApplicationController
   end
 
   def create
-    user = @current_user
-    boss = Boss.find(params[:boss_id])
-    damage = params[:damage_dealt].to_i
-    result = params[:result]
+    ActiveRecord::Base.transaction do
+      user = @current_user
+      boss = Boss.find(params[:boss_id])
+      damage = params[:damage_dealt].to_i
+      result = params[:result]
+      base_amount = params[:base_amount].to_i
 
-    # 累計ダメージを計算
-    total_damage = BossBattleLog.where(user: user, boss: boss).sum(:damage_dealt) + damage
-    remaining_hp = boss.hp - total_damage
+      # 累計ダメージを計算
+      total_damage = BossBattleLog.where(user: user, boss: boss).sum(:damage_dealt) + damage
+      remaining_hp = boss.hp - total_damage
 
-    BossBattleLog.create!(
-      user_id: user.id,
-      boss_id: boss.id,
-      damage_dealt: damage,
-      result: result
-    )
-
-    # if result && remaining_hp <= 0
-    #   user.coin.amount += 300
-    #   BossBattleLog.where(user: user, boss: boss).delete_all # 討伐成功時に累積ダメージをリセット
-    # elsif result
-    #   user.coin.amount += [10, 20, 30].sample
-    # end
-
-    # user.coin.save!
-
-    render json: { remaining_hp: remaining_hp, result: result }
+      BossBattleLog.create!(
+        user_id: user.id,
+        boss_id: boss.id,
+        damage_dealt: damage,
+        result: result
+        )
+        
+        gained_coins = user.gain_coins(base_amount)
+        # if result && remaining_hp <= 0
+        # #   BossBattleLog.where(user: user, boss: boss).delete_all # 討伐成功時に累積ダメージをリセット
+        # # end
+        user.special_mode_unlocked = false
+        user.save!
+        
+        render json: {
+          remaining_hp: remaining_hp,
+          result: result,
+          user: user.as_json(include: { coin: { only: [:amount] } }),
+          gained_coins: gained_coins
+        }
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
   end
-end
 end
 end
